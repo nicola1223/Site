239f3fb35d4ea2e52a25d0591df71b3482ec81f0
nicola <belchuk2005@mail.ru>
2025-01-17 16:44:49 +0300

    Trucks images album model

--- app/trucks/admin.py
+++ app/trucks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Truck
+from .models import Truck, TruckImage
 
 # Register your models here.
 admin.site.register(Truck)
+admin.site.register(TruckImage)
--- app/trucks/models.py
+++ app/trucks/models.py
@@ -3,9 +3,13 @@ from django.db import models
 # Create your models here.
 class Truck(models.Model):
     truck_name = models.CharField(max_length=30)
-    truck_image = models.ImageField(upload_to="truck_photos", null=True)
+    truck_main_image = models.ImageField(upload_to="truck_photos", null=True)
     truck_description = models.TextField(null=True)
     truck_price = models.FloatField()
 
     def __str__(self):
         return f"{self.truck_name}"
+
+class TruckImage(models.Model):
+    image = models.ImageField(upload_to="truck_photos", null=True)
+    truck = models.ForeignKey('truck', on_delete=models.CASCADE)
