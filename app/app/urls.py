22324a4eef3705a07ef2d8a08c908aa27634cf6b
nicola <belchuk2005@mail.ru>
2025-01-17 17:50:54 +0300

    Main urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("main.urls")),
     path("trucks", include("trucks.urls")),
     path('admin/', admin.site.urls),
 ]
