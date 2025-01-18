5737d6fe74091d12a549cd16756771e5367dc3f0
nicola <belchuk2005@mail.ru>
2025-01-18 20:52:13 +0300

    Truck Serializer

---
+++ app/trucks/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Truck
+
+class TruckSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Truck
+        fields = ('pk', 'truck_name', 'truck_main_image', 'truck_description', 'truck_price')
+
