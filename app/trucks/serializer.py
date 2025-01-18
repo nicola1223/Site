853fa61e749a0dd80e32ab2e0e5569e9ed1c8e1a
nicola <belchuk2005@mail.ru>
2025-01-18 20:57:32 +0300

    View packages init

--- app/trucks/serializer.py
+++ app/trucks/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Truck
+from .models import *
 
 class TruckSerializer(serializers.ModelSerializer):
 
--- app/trucks/views.py
+++ app/trucks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import *
+from .serializer import *
 
 # Create your views here.
