e165083ddc55c76c0e868272749db55d376a63d2
nicola <belchuk2005@mail.ru>
2025-01-20 10:51:32 +0300

    Truck urls fix

--- app/trucks/urls.py
+++ app/trucks/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.trucks_list, name='truck_list'),
-    path('?P<int:pk>', views.trucks_detail, name='truck_detail')
+    path('/<int:pk>/', views.trucks_detail, name='truck_detail')
 ]
