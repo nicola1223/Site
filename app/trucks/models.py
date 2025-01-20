b56dc646612012015e623adad20805460e0433b8
nicola <belchuk2005@mail.ru>
2025-01-20 10:54:49 +0300

    Truck model alter image field

--- app/trucks/models.py
+++ app/trucks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Truck(models.Model):
     truck_name = models.CharField(max_length=30)
-    truck_main_image = models.ImageField(upload_to="truck_photos", null=True)
+    truck_main_image = models.ImageField(upload_to="truck_photos", null=True, blank=True)
     truck_description = models.TextField(null=True)
     truck_price = models.FloatField()
 
