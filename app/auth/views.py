8ddde8fd581d994847351d4664bf1d44da7075f3
nicola <belchuk2005@mail.ru>
2025-01-24 13:16:16 +0300

    Revert "Auth app init"
    This reverts commit 651f7898beb23d67bed406144be963fccc8814a7.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
 
 # Create your views here.
-class ProtectedView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response({'message': 'Authenticated!'})
