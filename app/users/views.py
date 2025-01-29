04210ca6eade4c5d09394202bb9ffa91c36ca854
nicola <belchuk2005@mail.ru>
2025-01-29 17:57:23 +0300

    Add outstanding token

--- app/users/views.py
+++ app/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
 
 class ProtectedView(APIView):
@@ -19,9 +19,18 @@ class TokenDeleteView(APIView):
         try:
             refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
-            BlacklistedToken.objects.create(token=token)
+
+            outstanding_token = OutstandingToken.objects.get(
+                token=token.payload.get("jti"),
+                user_id=token.payload.get("user_id")
+            )
+
+            BlacklistedToken.objects.create(
+                token=outstanding_token,
+                user_id=token.payload.get("user_id")
+            )
             return Response(status=status.HTTP_205_RESET_CONTENT)
-        except TokenError:
+        except OutstandingToken.DoesNotExist:
             return Response(
                 {"error": "Invalid token"},
                 status=status.HTTP_400_BAD_REQUEST
