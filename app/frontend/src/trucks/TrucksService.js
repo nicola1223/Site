065643ec2198874c86a52845395fa81d23cde440
nicola <belchuk2005@mail.ru>
2025-02-06 17:29:35 +0300

    Api track auth

--- app/frontend/src/trucks/TrucksService.js
+++ app/frontend/src/trucks/TrucksService.js
@@ -1,29 +1,28 @@
-import axios from "axios";
-const API_URL = 'http://localhost:8000';
+import axios from "../api/axios";
 
 export default class TrucksService{
     getTrucks() {
-        const url = `${API_URL}/api/trucks/`;
+        const url = `/trucks/`;
         return axios.get(url).then(response => response.data);
     }
     getTruckByURL(link) {
-        const url = `${API_URL}${link}`;
+        const url = `${link}`;
         return axios.get(url).then(response => response.data);
     }
     getTruck(pk) {
-        const url = `${API_URL}/api/trucks/${pk}/`;
+        const url = `/trucks/${pk}/`;
         return axios.get(url).then(response => response.data);
     }
     deleteTruck(truck) {
-        const url = `${API_URL}/api/trucks/${truck.pk}/`;
+        const url = `/trucks/${truck.pk}/`;
         return axios.delete(url);
     }
     createTruck(truck) {
-        const url = `${API_URL}/api/trucks/`;
+        const url = `/trucks/`;
         return axios.post(url, truck);
     }
     updateTruck(truck) {
-        const url = `${API_URL}/api/trucks/${truck.get('pk')}/`;
+        const url = `/trucks/${truck.get('pk')}/`;
         return axios.put(url, truck);
     }
 }
